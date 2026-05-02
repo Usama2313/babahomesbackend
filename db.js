604505218103382019fc3f3489165fbaaa97b5f2
Syed Usama <syedusamatanveer@gmail.com>
2026-05-02 18:18:42 +0300

    Fix: resolve SSL self-signed certificate error

--- db.js
+++ db.js
@@ -5,12 +5,24 @@ const sequelize = (process.env.DATABASE_URL || process.env.POSTGRES_URL)
   ? new Sequelize((process.env.DATABASE_URL || process.env.POSTGRES_URL), {
     dialect: 'postgres',
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   })
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   });
 
 module.exports = sequelize;
